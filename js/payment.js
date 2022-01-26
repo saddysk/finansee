4f96685cf5eb48309ca78df8c2c08980c8b0514b
SaddySk <sksaddamh08@gmail.com>
2022-01-26 17:28:45 +0530

    update

--- js/payment.js
+++ js/payment.js
@@ -1,25 +1,21 @@
 // create an order
 function CreateOrder() {
   const url = "https://api.razorpay.com/v1/orders";
-
   data = {
     amount: 29900,
     currency: "INR",
     receipt: "finansee_test_payment",
   };
 
-  let param = {
-    method: "post",
-    headers: {
-      Authorization:
-        "Basic cnpwX3Rlc3RfNHVuaVZKYmtUTHZJbHM6eGRGVTczYjB6UUp0dUhNd0Zid21jVUVp",
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: data,
-  };
-
-  fetch(url, param)
+  fetch(url, {
+    method: "POST",
+    mode: "cors",
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+    Authorization:
+      "Basic cnpwX3Rlc3RfNHVuaVZKYmtUTHZJbHM6eGRGVTczYjB6UUp0dUhNd0Zid21jVUVp",
+    body: JSON.stringify(data),
+  })
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
